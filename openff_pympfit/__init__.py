1f7c6fefcf72cab1bcf60dd0cbbb8bfcd863d8bd
Shehan Parmar <shehanparmar@Shehans-MacBook-Pro.local>
2026-01-20 12:34:53 -0500

    Export key classes from top-level package for simplified   imports

--- openff_pympfit/__init__.py
+++ openff_pympfit/__init__.py
@@ -1,7 +1,18 @@
 """Partial atomic charge assignment via multipole moment-based fitting algorithm"""
 
-# Add imports here
-from .openff_pympfit import *
-
-
 from ._version import __version__
+
+from openff_pympfit.mpfit import generate_mpfit_charge_parameter
+from openff_pympfit.mpfit.solvers import MPFITSVDSolver
+from openff_pympfit.gdma import GDMASettings
+from openff_pympfit.gdma.psi4 import Psi4GDMAGenerator
+from openff_pympfit.gdma.storage import MoleculeGDMARecord
+
+__all__ = [
+    "__version__",
+    "generate_mpfit_charge_parameter",
+    "MPFITSVDSolver",
+    "GDMASettings",
+    "Psi4GDMAGenerator",
+    "MoleculeGDMARecord",
+]
